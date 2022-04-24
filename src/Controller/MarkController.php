23e5f641758b2ff923a1503a60d90e009cbffc5c
Nguyễn Hữu Mạnh <81838757+Manhdc01@users.noreply.github.com>
2022-04-25 01:05:24 +0700

    .

--- src/Controller/MarkController.php
+++ src/Controller/MarkController.php
@@ -2,17 +2,20 @@
 
 namespace App\Controller;
 
+use App\Entity\Mark;
+use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-
+#[Route('/mark')]
 class MarkController extends AbstractController
 {
-    #[Route('/mark', name: 'app_mark')]
-    public function index(): Response
+    #[Route('/', name: 'mark_index')]
+    public function markIndex(ManagerRegistry $registry)
     {
+        $marks = $registry->getRepository(Mark::class)->findAll();
         return $this->render('mark/index.html.twig', [
-            'controller_name' => 'MarkController',
+            'marks' => $marks
         ]);
     }
 }
