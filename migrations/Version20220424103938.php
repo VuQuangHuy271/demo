c6ba412d320f2affaea4c7d3dd8625a2263811e1
Nguyễn Hữu Mạnh <81838757+Manhdc01@users.noreply.github.com>
2022-04-24 17:41:40 +0700

    ff

--- migrations/Version20220424103938.php
+++ migrations/Version20220424103938.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20220424102709 extends AbstractMigration
+final class Version20220424103938 extends AbstractMigration
 {
     public function getDescription(): string
     {
