027b45273798c987df943a0166ab43cc634f2f33
Nguyenkien13082001 <kienntgbh190773@fpt.edu.vn>
2022-04-25 01:08:15 +0700

    update
    update

---
+++ src/Controller/StudentController.php
@@ -0,0 +1,93 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Student;
+use App\Form\StudentType;
+use Doctrine\Persistence\ManagerRegistry;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+class StudentController extends AbstractController
+{
+    #[Route('/', name: 'student_index')]
+    public function studentIndex(ManagerRegistry $registry)
+    {
+        $student = $registry->getRepository(Student::class)->findAll();  
+        return $this->render('student/index.html.twig', [
+            'student' => $student,
+        ]);
+    }
+
+    #[Route('/detail/{id}', name: 'student_detail')]
+    public function studentDetail (ManagerRegistry $registry, $id) {
+        $student = $registry->getRepository(Student::class)->find($id);
+        if ($student == null) {
+            $this->addFlash("Error","student not found !");
+            return $this->redirectToRoute("student_index");
+        }
+        return $this->render("student/detail.html.twig",
+        [
+            'student' => $student,
+        ]);
+    }
+
+
+    #[Route('/delete/{id}', name: 'student_delete')]
+    public function studentDelete (ManagerRegistry $registry, $id) {
+        $student = $registry->getRepository(Student::class)->find($id);
+        // $genres = $registry->getRepository(Genre::class)->findAll();
+        if ($student == null) {
+            $this->addFlash("Error","student not found !");
+        } else {
+            $manager = $registry->getManager();
+            $manager->remove($student);
+            $manager->flush();
+            $this->addFlash("Success", "student delete succeed !");
+        }
+        return $this->redirectToRoute("student_index",
+                        [
+                            'student' => $student,
+                        ]);
+    }
+
+    #[Route('/add', name: 'student_add')]
+    public function studentAdd(Request $request, ManagerRegistry $registry){
+        $student = new Student;
+        $form = $this->createForm(StudentType::class, $student);
+        $form->handleRequest($request);
+        if($form->isSubmitted() && $form->isValid()){
+            $manager = $registry->getManager();
+            $manager->persist($student);
+            $manager->flush();
+            $this->addFlash('Success', 'Student added');
+            return $this->redirectToRoute('student_index');
+        }
+        return $this->renderForm('student/add.html.twig', [
+            'StudentForm' => $form
+        ]);
+    }
+    
+    #[Route('/edit/{id}', name: 'student_edit')]
+    public function studentEdit(Request $request, ManagerRegistry $registry, $id){
+        $student = $registry->getRepository(Student::class)->find($id);
+        if($student == null){
+            $this->addFlash('Error', 'Student not found');
+            return $this->redirectToRoute('student_index');
+        }
+        $form = $this->createForm(StudentType::class, $student);
+        $form->handleRequest($request);
+        if($form->isSubmitted() && $form->isValid()){
+            $manager = $registry->getManager();
+            $manager->persist($student);
+            $manager->flush();
+            $this->addFlash('Success', 'Student edited');
+            return $this->redirectToRoute('student_index');
+        }
+        return $this->renderForm('student/edit.html.twig', [
+            'StudentForm' => $form
+        ]);
+    }
+}
---
+++ src/Form/StudentType.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Student;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+
+class StudentType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('Name', TextType::class, 
+            [
+                'label' => 'Name',
+                'attr' => [
+                    'placeholder' => 'Enter your name',
+                ],
+                'required' => true,
+            ])
+                
+                ->add('Gender', TextType::class,[
+                    'label'=> 'Gender',
+                    'required' => true,
+
+                ])
+                ->add('DateOfBirth', DateType::class,
+                [
+
+                    'required' => true,
+                    'widget' => 'single_text'
+                ])
+                ->add('Phone', TextType::class,
+                [
+                    'label' => 'Phone',
+                    'required' => true,
+                ])
+                ->add('Email', TextType::class,
+                [
+                    'label' => 'Email',
+                    'required' => true,
+                ])
+                ->add('Image', TextType::class,
+                [
+                    'label' => 'Image',
+                    'required' => true,
+                ])
+                ->add('save', SubmitType::class)
+                ;
+
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Student::class,
+        ]);
+    }
+}
