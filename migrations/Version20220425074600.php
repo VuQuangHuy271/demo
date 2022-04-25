646f3e01b4193519d7e76f1c886fed53770ec66f
Nguyễn Hữu Mạnh <81838757+Manhdc01@users.noreply.github.com>
2022-04-25 14:57:10 +0700

    okok

--- migrations/Version20220425074600.php
+++ migrations/Version20220425074600.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20220424213110 extends AbstractMigration
+final class Version20220425074600 extends AbstractMigration
 {
     public function getDescription(): string
     {
@@ -20,20 +20,16 @@ final class Version20220424213110 extends AbstractMigration
     public function up(Schema $schema): void
     {
         // this up() migration is auto-generated, please modify it to your needs
-        $this->addSql('ALTER TABLE mark ADD assignment_2 DOUBLE PRECISION NOT NULL, CHANGE mark assignment_1 DOUBLE PRECISION NOT NULL');
         $this->addSql('ALTER TABLE student ADD course_id INT DEFAULT NULL');
         $this->addSql('ALTER TABLE student ADD CONSTRAINT FK_B723AF33591CC992 FOREIGN KEY (course_id) REFERENCES course (id)');
         $this->addSql('CREATE INDEX IDX_B723AF33591CC992 ON student (course_id)');
-        $this->addSql('ALTER TABLE subject ADD image VARCHAR(255) NOT NULL');
     }
 
     public function down(Schema $schema): void
     {
         // this down() migration is auto-generated, please modify it to your needs
-        $this->addSql('ALTER TABLE mark ADD mark DOUBLE PRECISION NOT NULL, DROP assignment_1, DROP assignment_2');
         $this->addSql('ALTER TABLE student DROP FOREIGN KEY FK_B723AF33591CC992');
         $this->addSql('DROP INDEX IDX_B723AF33591CC992 ON student');
         $this->addSql('ALTER TABLE student DROP course_id');
-        $this->addSql('ALTER TABLE subject DROP image');
     }
 }
